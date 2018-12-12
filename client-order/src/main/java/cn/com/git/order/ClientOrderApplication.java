6e983a4dbf4addaeffb8ace17315d4a866d2a31c
zhangzhenfu <zzf610503732@163.com>
2018-12-12 14:13:57 +0800

    server-hystrix-dashboard

--- client-order/src/main/java/cn/com/git/order/ClientOrderApplication.java
+++ client-order/src/main/java/cn/com/git/order/ClientOrderApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.feign.FeignClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 
+//开启断路器，用于监控识别
+//访问 http://localhost:7002/hystrix.stream 查看断路器状态
+@EnableHystrix
 //通过注解@EnableEurekaClient 表明自己是一个eurekaclient. 低版本用的是 @EnableDiscoveryClient 注解
 @EnableEurekaClient
 //支持 feign 的注解
