78962ea86fe2b3dad5e37c44fd72824a025b7c60
zhangzhenfu <zzf610503732@163.com>
2018-12-21 17:27:19 +0800

    stream

---
+++ client-goods/src/main/java/cn/com/git/goods/configuration/RedisConfig.java
@@ -0,0 +1,23 @@
+package cn.com.git.goods.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * Redis缓存配置类
+ *
+ */
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    @SuppressWarnings("all")
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory) {
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+        return template;
+    }
+
+}
--- client-goods/src/main/java/cn/com/git/goods/service/GoodsService.java
+++ client-goods/src/main/java/cn/com/git/goods/service/GoodsService.java
@@ -1,5 +1,11 @@
 package cn.com.git.goods.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataAccessException;
+import org.springframework.data.redis.connection.RedisConnection;
+import org.springframework.data.redis.core.RedisCallback;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +15,36 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class GoodsService {
+
+    @Autowired
+    private RedisTemplate<String,Object> redisTemplate ;
+
     /**
      * 通过商品代码获取商品详情
      * @return
      */
     @RequestMapping(value = "/goodsDetail")
-    public String goodsDetail(@RequestParam("code") int code){
+    public String goodsDetail(@RequestParam("code") String code){
         //测试调用，写死即可
         return "商品代码：" + code ;
+    }
+
+    @RequestMapping(value = "/getGoods")
+    public String getGoods(@RequestParam("code") String code){
+        String val = (String)redisTemplate.execute(new RedisCallback() {
+            @Override
+            public String doInRedis(RedisConnection redisConnection) throws DataAccessException {
+
+                RedisSerializer<String> serializer = redisTemplate.getStringSerializer();
+                byte[] key = serializer.serialize(code);
+                byte[] value = redisConnection.get(key);
+                if (value == null) {
+                    return null;
+                }
+                return serializer.deserialize(value);
+            }
+        });
+
+        return val ;
     }
 }
